cf3760c420d34c16e74eeda61224d433d5418584
Natiq <natiq00h2272@gmail.com>
2023-03-05 13:44:39 +0400

    UI - Home Navigation and Screens created

---
+++ app/src/main/java/com/natiqhaciyef/wordlearningapp/view/screens/home/NotificationScreen.kt
@@ -0,0 +1,9 @@
+package com.natiqhaciyef.wordlearningapp.view.screens.home
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotificationScreen(){
+    Text(text = "Notification")
+}
