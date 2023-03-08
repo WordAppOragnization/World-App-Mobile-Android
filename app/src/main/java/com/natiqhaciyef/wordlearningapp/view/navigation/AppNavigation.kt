33ab9bd30554aaa6901cc6669cad16944b222c6b
Natiq <natiq00h2272@gmail.com>
2023-03-08 15:39:24 +0400

    UI - Home Screens Group cards implemented and added functionality

--- app/src/main/java/com/natiqhaciyef/wordlearningapp/view/navigation/AppNavigation.kt
+++ app/src/main/java/com/natiqhaciyef/wordlearningapp/view/navigation/AppNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.natiqhaciyef.wordlearningapp.view.screens.home.MainScreen
+import com.natiqhaciyef.wordlearningapp.view.screens.home.WordsScreen
 import com.natiqhaciyef.wordlearningapp.view.screens.registration.LoginScreen
 import com.natiqhaciyef.wordlearningapp.view.screens.registration.RegisterScreen
 import com.natiqhaciyef.wordlearningapp.view.screens.registration.ResetPasswordScreen
@@ -26,8 +27,12 @@ fun AppNavigation(){
         }
 
         composable(ScreenID.HomeScreen.name){
-            MainScreen()
+            MainScreen(navController)
+        }
+
+        // Home Navigation
+        composable(ScreenID.WordsScreen.name){
+            WordsScreen()
         }
     }
-
 }
---
+++ app/src/main/java/com/natiqhaciyef/wordlearningapp/view/screens/home/WordsScreen.kt
@@ -0,0 +1,8 @@
+package com.natiqhaciyef.wordlearningapp.view.screens.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WordsScreen(){
+
+}
